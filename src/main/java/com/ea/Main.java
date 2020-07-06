daf5c6e4bc2a9038b1489db3b25df8f932d1caf0
MAHATRANGA TAHIANA <56284846+mtahiana@users.noreply.github.com>
2020-07-06 17:23:34 +0400

    Update Main.java

--- src/main/java/com/ea/Main.java
+++ src/main/java/com/ea/Main.java
@@ -30,7 +30,7 @@ public class Main {
     public static void main(String[] args) {
 
         //Selenium 4
-        System.setProperty("webdriver.chrome.driver", "C:\\driver\\Web\\chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/Tahiana/ChromeDriver/chromedriver");
         var chromeDriver = new ChromeDriver();
 
         var chromeDevTools = chromeDriver.getDevTools();
