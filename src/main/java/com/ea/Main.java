213b7542b0ad177daba0cb73af9ae2bee4db8ee8
MAHATRANGA TAHIANA <56284846+mtahiana@users.noreply.github.com>
2020-07-06 17:45:44 +0400

    Update Main.java

--- src/main/java/com/ea/Main.java
+++ src/main/java/com/ea/Main.java
@@ -24,6 +24,7 @@ import static org.openqa.selenium.devtools.network.Network.emulateNetworkConditi
 import static org.openqa.selenium.devtools.network.Network.loadingFailed;
 import static org.openqa.selenium.devtools.target.Target.attachToTarget;
 import static org.openqa.selenium.support.locators.RelativeLocator.withTagName;
+import static org.openqa.selenium.devtools.DevTools;
 
 public class Main {
 
@@ -31,9 +32,12 @@ public class Main {
 
         //Selenium 4
         System.setProperty("webdriver.chrome.driver", "/Users/Tahiana/ChromeDriver/chromedriver");
-        var chromeDriver = new ChromeDriver();
+        //var chromeDriver = new ChromeDriver();
+        ChromeDriver chromeDriver = new ChromeDriver();
 
-        var chromeDevTools = chromeDriver.getDevTools();
+        //var chromeDevTools = chromeDriver.getDevTools();
+        DevTools chromeDevTools = chromeDriver.getDevTools();
+        
         //Session of ChromeDevTool
         chromeDevTools.createSession();
 
